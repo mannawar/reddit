e0d0e31b185e7cb9ea8550a2204651fa7802a3ab
Mannawar Hussain <dearmannawar@gmail.com>
2020-06-20 04:59:20 +0300

    second committ

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ searchForm.addEventListener('submit', e => {
 
     //search Reddit
     let search = function(searchTerm, searchLimit, sortBy) {
-    return fetch(`http://www.reddit.com/search.json?q=${searchTerm}&limit=${searchLimit}&sort=${sortBy}`)
+    return fetch(`https://www.reddit.com/search.json?q=${searchTerm}&limit=${searchLimit}&sort=${sortBy}`)
     .then(res => res.json())
     .then(data => data.data.children.map(data => data.data))
     .catch(err => console.log(err));
